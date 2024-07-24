45f75f740c4fde1f7eb19931d9cc428de04ea622
Sophia_YU <59725384+Sophia36912@users.noreply.github.com>
2024-07-25 02:55:08 +0800

    Create bt.js

---
+++ modules/backend-tools/bt.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const User = mongoose.model('User', { name: String, email: String });
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
